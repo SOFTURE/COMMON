5100203caa3ea0bcefe5fdf604cda6321c1c0278
Jarosław Armatys <jarekarmatys@gmail.com>
2026-01-13 10:02:45 +0100

    feat: update IBaseRepository with new async methods for retrieval

--- COMMON/SOFTURE.Domain/Interfaces/IBaseRepository.cs
+++ COMMON/SOFTURE.Domain/Interfaces/IBaseRepository.cs
@@ -8,12 +8,13 @@ public interface IBaseRepository<TEntity, in TIdentifier>
     where TEntity : class, IEntity, IAggregateRoot
     where TIdentifier : class, IIdentifier
 {
+    Task<Maybe<TEntity>> GetByIdAsync(TIdentifier id);
+    Task<Maybe<IEnumerable<TEntity>>> GetAllAsync();
     Task<Result<TEntity>> AddAsync(TEntity entity);
     Task<Result<TEntity>> UpdateAsync(TEntity entity);
     Task<Result> RemoveAsync(TEntity entity);
+    
     Task<Maybe<IEnumerable<TEntity>>> FindAsync(Expression<Func<TEntity, bool>> predicate);
-    Task<Maybe<TEntity>> GetByIdAsync(TIdentifier id);
-    Task<Maybe<IEnumerable<TEntity>>> GetAllAsync();
-    Task<Maybe<IEnumerable<TEntity>>> PaginateAsync(int page, int elements);
+    Task<Maybe<IEnumerable<TEntity>>> PaginateAsync(int page, int elements, Expression<Func<TEntity, bool>> predicate);
     Task<Result<int>> CountAsync();
 }
