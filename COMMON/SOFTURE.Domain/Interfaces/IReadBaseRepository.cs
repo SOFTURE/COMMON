366c65daa6d0e3faedc035d1365b2e3b44a5d3b9
Jarosław Armatys <jarekarmatys@gmail.com>
2026-02-14 15:50:18 +0100

    Added: Task<Maybe<TEntity>> GetAsync(TIdentifier id);

--- COMMON/SOFTURE.Domain/Interfaces/IReadBaseRepository.cs
+++ COMMON/SOFTURE.Domain/Interfaces/IReadBaseRepository.cs
@@ -4,9 +4,10 @@ using SOFTURE.Language.Common;
 
 namespace SOFTURE.Domain.Interfaces;
 
-public interface IReadBaseRepository<TEntity>
+public interface IReadBaseRepository<TEntity, in TIdentifier>
     where TEntity : class, IEntity
 {
+    Task<Maybe<TEntity>> GetAsync(TIdentifier id);
     Task<Maybe<IEnumerable<TEntity>>> GetAllAsync();
     Task<Maybe<IEnumerable<TEntity>>> FindAsync(Expression<Func<TEntity, bool>> predicate);
     Task<Maybe<IEnumerable<TEntity>>> PaginateAsync(int page, int elements, Expression<Func<TEntity, bool>> predicate);
